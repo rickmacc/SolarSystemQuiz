dcad5bcfc227f027407b9f2d1ebb98c8479d2baf
Rick MacConnell <rick@eightfoldpath.org>
2016-12-04 15:43:26 -0500

    Added if/else conditional and toast message.

--- app/src/main/java/org/eightfoldpath/solarsystemquiz/MainActivity.java
+++ app/src/main/java/org/eightfoldpath/solarsystemquiz/MainActivity.java
@@ -1,5 +1,6 @@
 package org.eightfoldpath.solarsystemquiz;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,10 +9,12 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String EARTH = "earth";
+    private final static int HIGH_SCORE = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +25,18 @@ public class MainActivity extends AppCompatActivity {
     public void scoreQuiz(View view) {
         int totalCorrect = getTotalCorrect();
         Resources res = getResources();
-        String resultMessage = res.getString(R.string.result_message) + " " + totalCorrect;
+        String toastMessage = "";
+        if (totalCorrect == HIGH_SCORE) {
+            toastMessage += "\n" + res.getString(R.string.expert_message);
+        } else {
+            toastMessage += "\n" + res.getString(R.string.try_again_message);
+        }
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT);
+        toast.show();
+
         TextView quizResult = (TextView) findViewById(R.id.quiz_result);
+        String resultMessage = res.getString(R.string.result_message) + " " + totalCorrect;
         quizResult.setText(resultMessage);
     }
 
@@ -36,17 +49,17 @@ public class MainActivity extends AppCompatActivity {
 
         String planet = ((TextView) findViewById(R.id.third_planet_answer)).getText().toString();
         Log.i("MainActivity.java", "Third planet :" + planet);
-        if (planet.toLowerCase().equals(EARTH)) totalCorrect += 1;
+        if (planet.toLowerCase().equals(EARTH)) totalCorrect++;
 
         boolean mercuryMoons = ((CheckBox) findViewById(R.id.mercury_moons)).isChecked();
         boolean venusMoons = ((CheckBox) findViewById(R.id.venus_moons)).isChecked();
         boolean jupiterMoons = ((CheckBox) findViewById(R.id.jupiter_moons)).isChecked();
         if (mercuryMoons && venusMoons && !jupiterMoons) {
-            totalCorrect += 1;
+            totalCorrect++;
         }
 
         boolean saturnRings = ((RadioButton) findViewById(R.id.radio_saturn_rings)).isChecked();
-        if (saturnRings) totalCorrect += 1;
+        if (saturnRings) totalCorrect++;
 
         return totalCorrect;
 
